2b5d04b1fc92b9ce565d3cd91f32db5128474e43
Andre Kohn <andre@kohn.io>
2018-10-23 12:27:55 +0200

    Add task 2

--- README.md
+++ README.md
@@ -30,5 +30,47 @@ Useful links:
 * [TPC-C specification](http://www.tpc.org/tpc_documents_current_versions/pdf/tpc-c_v5.11.0.pdf)
 * [TPC-C data (5 warehouses)](https://db.in.tum.de/teaching/ws1718/imlab/tpcc_5w.tar.gz)
 
+## Task 2
+
+* Use *bison* and *flex* to write a parser for SQL schema files.
+    * You can assume, that all attributes are declared NOT NULL.
+    * You must support primary keys.
+    * You can ignore secondary indexes (for now).
+* Write a schema compiler, that generates C++ code from the [schema representation](include/imlab/schemac/schema_parse_context.h).
+    * You must implement the CRUD operations for every relation. (**C**reate, **R**ead, **U**pdate, **D**elete).
+    * It must further be possible to lookup a tuple by its primary key (if any).
+    * Use the schema compiler to generate code for the [schema](data/schema.sql) and add it to your repository.
+* Re-implement the [New Order](data/new_order.sql) transaction as [C++ function](include/imlab/database.h) using your generated code.
+
+The project skeleton for this task contains a grammar & scanner for a toy format called **FOO**, which looks as follows:
+
+```
+foo bar1 {
+    a integer,
+    b char{20}
+};
+
+foo bar2 {
+    c integer
+};
+```
+
+You can use the executable `schemac` to parse a **FOO** file with the following command:
+```
+./schemac --in ~/Desktop/example.foo --out_cc /tmp/example.cc --out_h /tmp/example.h
+```
+
+First learn how bison & flex work together and how they are used in the following files:
+* [FOO grammar](tools/schemac/schema_parser.y)
+* [FOO scanner](tools/schemac/schema_scanner.l)
+* [CMake integration](tools/schemac/local.cmake)
+* [Parse context H](include/imlab/schemac/schema_parse_context.h)
+* [Parse context CC](tools/schemac/schema_parse_context.cc)
+* [Parser tests](test/schemac/schema_parser_test.cc)
+
+Then use the parsed schema to generate C++ code with a schema compiler:
+* [Schema compiler H](include/imlab/schemac/schema_compiler.h)
+* [Schema compiler CC](tools/schemac/schema_compiler.cc)
+
 Make sure your builds are not failing! <br/>
 *Left Sidebar > CI /CD > Pipelines*
---
+++ tools/schemac/schema_parse_context.cc
@@ -0,0 +1,87 @@
+// ---------------------------------------------------------------------------------------------------
+// IMLAB
+// ---------------------------------------------------------------------------------------------------
+#include "imlab/schemac/schema_parse_context.h"
+#include "./gen/schema_parser.h"
+#include <sstream>
+#include <unordered_set>
+#include "imlab/infra/error.h"
+// ---------------------------------------------------------------------------------------------------
+using SchemaParseContext = imlab::schemac::SchemaParseContext;
+using Table = imlab::schemac::Table;
+using Schema = imlab::schemac::Schema;
+using Type = imlab::schemac::Type;
+// ---------------------------------------------------------------------------------------------------
+Type Type::Integer()    { Type t; t.tclass = kInteger; return t; }
+Type Type::Timestamp()  { Type t; t.tclass = kTimestamp; return t; }
+Type Type::Numeric(unsigned length, unsigned precision) {
+    Type t;
+    t.tclass = kNumeric;
+    t.length = length;
+    t.precision = precision;
+    return t;
+}
+Type Type::Char(unsigned length) {
+    Type t;
+    t.tclass = kChar;
+    t.length = length;
+    return t;
+}
+Type Type::Varchar(unsigned length) {
+    Type t;
+    t.tclass = kVarchar;
+    t.length = length;
+    return t;
+}
+// ---------------------------------------------------------------------------------------------------
+const char *Type::Name() const {
+    switch (tclass) {
+        case kInteger:      return "Integer";
+        case kTimestamp:    return "Timestamp";
+        case kNumeric:      return "Numeric";
+        case kChar:         return "Character";
+        case kVarchar:      return "Varchar";
+        default:            return "Unknown";
+    }
+}
+// ---------------------------------------------------------------------------------------------------
+// Constructor
+SchemaParseContext::SchemaParseContext(bool trace_scanning, bool trace_parsing)
+    : trace_scanning_(trace_scanning), trace_parsing_(trace_parsing) {}
+// ---------------------------------------------------------------------------------------------------
+// Destructor
+SchemaParseContext::~SchemaParseContext() {}
+// ---------------------------------------------------------------------------------------------------
+// Parse a string
+Schema SchemaParseContext::Parse(std::istream &in) {
+    beginScan(in);
+    imlab::schemac::SchemaParser parser(*this);
+    parser.set_debug_level(trace_parsing_);
+    parser.parse();
+    endScan();
+
+    // TODO
+    return {};
+}
+// ---------------------------------------------------------------------------------------------------
+// Yield an error
+void SchemaParseContext::Error(const std::string& m) {
+    throw SchemaCompilationError(m);
+}
+// ---------------------------------------------------------------------------------------------------
+// Yield an error
+void SchemaParseContext::Error(uint32_t line, uint32_t column, const std::string &err) {
+    std::stringstream ss;
+    ss << "[ l=" << line << " c=" << column << " ] " << err << std::endl;
+    throw SchemaCompilationError(ss.str());
+}
+// ---------------------------------------------------------------------------------------------------
+// Define a table
+void SchemaParseContext::defineFoo(const std::string &id, const std::vector<SomeDeclaration> &declarations) {
+    std::cout << "FOO " << id << std::endl;
+    for (auto &decl : declarations) {
+        std::cout << "    " << decl.id << " " << decl.type.Name() << std::endl;
+    }
+}
+// ---------------------------------------------------------------------------------------------------
+
