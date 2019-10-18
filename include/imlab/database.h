2fc1a31c47514a3dbb56ad19fc729480cb62d1fe
Andreas Zimmerer <andreas.zimmerer@tum.de>
2019-10-18 23:36:58 +0200

    implemented load data for all tables

--- include/imlab/database.h
+++ include/imlab/database.h
@@ -138,6 +138,64 @@ struct CustomerTable {
 
     std::unordered_map<Key<Integer, Integer, Integer>, uint64_t> index_prim_key;
     std::unordered_map<Key<Integer, Integer, Varchar<16>, Varchar<16>>, uint64_t> index_wdl;
+
+    void append_row(Integer c_id_val, Integer c_d_id_val, Integer c_w_id_val,
+            Varchar<16> c_first_val, Char<2> c_middle_val, Varchar<16> c_last_val,
+            Varchar<20> c_street_1_val, Varchar<20> c_street_2_val, Varchar<20> c_city_val,
+            Char<2> c_state_val, Char<9> c_zip_val, Char<16> c_phone_val, Timestamp c_since_val,
+            Char<2> c_credit_val, Numeric<12, 2> c_credit_lim_val, Numeric<4, 4> c_discount_val,
+            Numeric<12, 2> c_balance_val, Numeric<12, 2> c_ytd_paymenr_val,
+            Numeric<4, 0> c_payment_cnt_val, Numeric<4, 0> c_delivery_cnt_val, Varchar<500> c_data_val) {
+
+        c_id.push_back(c_id_val);
+        c_d_id.push_back(c_d_id_val);
+        c_w_id.push_back(c_w_id_val);
+        c_first.push_back(c_first_val);
+        c_middle.push_back(c_middle_val);
+        c_last.push_back(c_last_val);
+        c_street_1.push_back(c_street_1_val);
+        c_street_2.push_back(c_street_2_val);
+        c_city.push_back(c_city_val);
+        c_state.push_back(c_state_val);
+        c_zip.push_back(c_zip_val);
+        c_phone.push_back(c_phone_val);
+        c_since.push_back(c_since_val);
+        c_credit.push_back(c_credit_val);
+        c_credit_lim.push_back(c_credit_lim_val);
+        c_discount.push_back(c_discount_val);
+        c_balance.push_back(c_balance_val);
+        c_ytd_paymenr.push_back(c_ytd_paymenr_val);
+        c_payment_cnt.push_back(c_payment_cnt_val);
+        c_delivery_cnt.push_back(c_delivery_cnt_val);
+
+        index_prim_key[Key(c_w_id_val, c_d_id_val, c_id_val)] = size;
+        index_wdl[Key(c_w_id_val, c_d_id_val, c_last_val, c_first_val)] = size;
+
+        size++;
+
+        assert(c_id.size() == size
+        && c_d_id.size() == size
+        && c_w_id.size() == size
+        && c_first.size() == size
+        && c_middle.size() == size
+        && c_last.size() == size
+        && c_street_1.size() == size
+        && c_street_2.size() == size
+        && c_city.size() == size
+        && c_state.size() == size
+        && c_zip.size() == size
+        && c_phone.size() == size
+        && c_since.size() == size
+        && c_credit.size() == size
+        && c_credit_lim.size() == size
+        && c_discount.size() == size
+        && c_balance.size() == size
+        && c_ytd_paymenr.size() == size
+        && c_payment_cnt.size() == size
+        && c_delivery_cnt.size() == size
+        && index_prim_key.size() == size
+        && index_wdl.size() == size);
+    }
 };
 struct HistoryTable {
     uint64_t size = 0;
@@ -150,6 +208,31 @@ struct HistoryTable {
     std::vector<Timestamp> h_date;
     std::vector<Numeric<6, 2>> h_amount;
     std::vector<Varchar<24>> h_data;
+
+    void append_row(Integer h_c_id_val, Integer h_c_d_id_val, Integer h_c_w_id_val,
+            Integer h_d_id_val, Integer h_w_id_val, Timestamp h_date_val,
+            Numeric<6, 2> h_amount_val, Varchar<24> h_data_val) {
+
+        h_c_id.push_back(h_c_id_val);
+        h_c_d_id.push_back(h_c_d_id_val);
+        h_c_w_id.push_back(h_c_w_id_val);
+        h_d_id.push_back(h_d_id_val);
+        h_w_id.push_back(h_w_id_val);
+        h_date.push_back(h_date_val);
+        h_amount.push_back(h_amount_val);
+        h_data.push_back(h_data_val);
+
+        size++;
+
+        assert(h_c_id.size() == size
+        && h_c_d_id.size() == size
+        && h_c_w_id.size() == size
+        && h_d_id.size() == size
+        && h_w_id.size() == size
+        && h_date.size() == size
+        && h_amount.size() == size
+        && h_data.size() == size);
+    }
 };
 struct NeworderTable {
     uint64_t size = 0;
@@ -190,6 +273,36 @@ struct OrderTable {
 
     std::unordered_map<Key<Integer, Integer, Integer>, uint64_t> index_prim_key;
     std::unordered_map<Key<Integer, Integer, Integer, Integer>, uint64_t> index_wdc;
+
+    void append_row(Integer o_id_val, Integer o_d_id_val, Integer o_w_id_val,
+            Integer o_c_id_val, Timestamp o_entry_d_val, Integer o_carrier_id_val,
+            Numeric<2, 0> o_ol_cnt_val, Numeric<1, 0> o_all_local_val) {
+
+        o_id.push_back(o_id_val);
+        o_d_id.push_back(o_d_id_val);
+        o_w_id.push_back(o_w_id_val);
+        o_c_id.push_back(o_c_id_val);
+        o_entry_d.push_back(o_entry_d_val);
+        o_carrier_id.push_back(o_carrier_id_val);
+        o_ol_cnt.push_back(o_ol_cnt_val);
+        o_all_local.push_back(o_all_local_val);
+
+        index_prim_key[Key(o_w_id_val, o_d_id_val, o_id_val)] = size;
+        index_wdc[Key(o_w_id_val, o_d_id_val, o_c_id_val, o_id_val)] = size;
+
+        size++;
+
+        assert(o_id.size() == size
+        && o_d_id.size() == size
+        && o_w_id.size() == size
+        && o_c_id.size() == size
+        && o_entry_d.size() == size
+        && o_carrier_id.size() == size
+        && o_ol_cnt.size() == size
+        && o_all_local.size() == size
+        && index_prim_key.size() == size
+        && index_wdc.size() == size);
+    }
 };
 struct OrderlineTable {
     uint64_t size = 0;
@@ -206,6 +319,39 @@ struct OrderlineTable {
     std::vector<Char<24>> ol_dist_info;
 
     std::unordered_map<Key<Integer, Integer, Integer, Integer>, uint64_t> index_prim_key;
+
+    void append_row(Integer ol_o_id_val, Integer ol_d_id_val, Integer ol_w_id_val,
+            Integer ol_number_val, Integer ol_i_id_val, Integer ol_supply_w_id_val,
+            Timestamp ol_delivery_d_val, Numeric<2, 0> ol_quantity_val,
+            Numeric<6, 2> ol_amount_val, Char<24> ol_dist_info_val) {
+
+        ol_o_id.push_back(ol_o_id_val);
+        ol_d_id.push_back(ol_d_id_val);
+        ol_w_id.push_back(ol_w_id_val);
+        ol_number.push_back(ol_number_val);
+        ol_i_id.push_back(ol_i_id_val);
+        ol_supply_w_id.push_back(ol_supply_w_id_val);
+        ol_delivery_d.push_back(ol_delivery_d_val);
+        ol_quantity.push_back(ol_quantity_val);
+        ol_amount.push_back(ol_amount_val);
+        ol_dist_info.push_back(ol_dist_info_val);
+
+        index_prim_key[Key(ol_w_id_val, ol_d_id_val, ol_o_id_val, ol_number_val)] = size;
+
+        size++;
+
+        assert(ol_i_id.size() == size
+        && ol_d_id.size() == size
+        && ol_w_id.size() == size
+        && ol_number.size() == size
+        && ol_i_id.size() == size
+        && ol_supply_w_id.size() == size
+        && ol_delivery_d.size() == size
+        && ol_quantity.size() == size
+        && ol_amount.size() == size
+        && ol_dist_info.size() == size
+        && index_prim_key.size() == size);
+    }
 };
 struct ItemTable {
     uint64_t size = 0;
@@ -261,6 +407,55 @@ struct StockTable {
     std::vector<Varchar<50>> s_data;
 
     std::unordered_map<Key<Integer, Integer>, uint64_t> index_prim_key;
+
+    void append_row(Integer s_i_id_val, Integer s_w_id_val, Numeric<4, 0> s_quantity_val,
+            Char<24> s_dist_01_val, Char<24> s_dist_02_val, Char<24> s_dist_03_val,
+            Char<24> s_dist_04_val, Char<24> s_dist_05_val, Char<24> s_dist_06_val,
+            Char<24> s_dist_07_val, Char<24> s_dist_08_val, Char<24> s_dist_09_val,
+            Char<24> s_dist_10_val, Numeric<8, 0> s_ytd_val, Numeric<4, 0> s_order_cnt_val,
+            Numeric<4, 0> s_remote_cnt_val, Varchar<50> s_data_val) {
+
+        s_i_id.push_back(s_i_id_val);
+        s_w_id.push_back(s_w_id_val);
+        s_quantity.push_back(s_quantity_val);
+        s_dist_01.push_back(s_dist_01_val);
+        s_dist_02.push_back(s_dist_02_val);
+        s_dist_03.push_back(s_dist_03_val);
+        s_dist_04.push_back(s_dist_04_val);
+        s_dist_05.push_back(s_dist_05_val);
+        s_dist_06.push_back(s_dist_06_val);
+        s_dist_07.push_back(s_dist_07_val);
+        s_dist_08.push_back(s_dist_08_val);
+        s_dist_09.push_back(s_dist_09_val);
+        s_dist_10.push_back(s_dist_10_val);
+        s_ytd.push_back(s_ytd_val);
+        s_order_cnt.push_back(s_order_cnt_val);
+        s_remote_cnt.push_back(s_remote_cnt_val);
+        s_data.push_back(s_data_val);
+
+        index_prim_key[Key(s_w_id_val, s_i_id_val)] = size;
+
+        size++;
+
+        assert(s_i_id.size() == size
+        && s_w_id.size() == size
+        && s_quantity.size() == size
+        && s_dist_01.size() == size
+        && s_dist_02.size() == size
+        && s_dist_03.size() == size
+        && s_dist_04.size() == size
+        && s_dist_05.size() == size
+        && s_dist_06.size() == size
+        && s_dist_07.size() == size
+        && s_dist_08.size() == size
+        && s_dist_09.size() == size
+        && s_dist_10.size() == size
+        && s_ytd.size() == size
+        && s_order_cnt.size() == size
+        && s_remote_cnt.size() == size
+        && s_data.size() == size
+        && index_prim_key.size() == size);
+    }
 };
 
 
--- src/database.cc
+++ src/database.cc
@@ -6,86 +6,159 @@
 #include <array>
 #include "imlab/infra/error.h"
 #include "imlab/infra/types.h"
+#include "../include/imlab/infra/types.h"
 
 namespace imlab {
 
 void Database::LoadWarehouse(std::istream &in) {
     std::string tmp = "";
 
-    while(in.peek() != EOF) {
-        std::getline(in, tmp,'|');
+    while (in.peek() != EOF) {
+        std::getline(in, tmp, '|');
         auto w_id = Integer::castString(tmp.c_str(), tmp.size());
-        std::getline(in, tmp,'|');
+        std::getline(in, tmp, '|');
         auto w_name = Varchar<10>::castString(tmp.c_str(), tmp.size());
-        std::getline(in, tmp,'|');
+        std::getline(in, tmp, '|');
         auto w_street_1 = Varchar<20>::castString(tmp.c_str(), tmp.size());
-        std::getline(in, tmp,'|');
+        std::getline(in, tmp, '|');
         auto w_street_2 = Varchar<20>::castString(tmp.c_str(), tmp.size());
-        std::getline(in, tmp,'|');
+        std::getline(in, tmp, '|');
         auto w_city = Varchar<20>::castString(tmp.c_str(), tmp.size());
-        std::getline(in, tmp,'|');
+        std::getline(in, tmp, '|');
         auto w_state = Char<2>::castString(tmp.c_str(), tmp.size());
-        std::getline(in, tmp,'|');
+        std::getline(in, tmp, '|');
         auto w_zip = Char<9>::castString(tmp.c_str(), tmp.size());
-        std::getline(in, tmp,'|');
+        std::getline(in, tmp, '|');
         auto w_tax = Numeric<4, 4>::castString(tmp.c_str(), tmp.size());
-        std::getline(in, tmp,'\n');
+        std::getline(in, tmp, '\n');
         auto w_ytd = Numeric<12, 2>::castString(tmp.c_str(), tmp.size());
 
         warehouseTable.append_row(w_id, w_name, w_street_1, w_street_2, w_city,
-                w_state, w_zip, w_tax, w_ytd);
+                                  w_state, w_zip, w_tax, w_ytd);
     }
 }
 
 void Database::LoadDistrict(std::istream &in) {
     std::string tmp = "";
 
-    while(in.peek() != EOF) {
-        std::getline(in, tmp,'|');
+    while (in.peek() != EOF) {
+        std::getline(in, tmp, '|');
         auto d_id = Integer::castString(tmp.c_str(), tmp.size());
-        std::getline(in, tmp,'|');
+        std::getline(in, tmp, '|');
         auto d_w_id = Integer::castString(tmp.c_str(), tmp.size());
-        std::getline(in, tmp,'|');
+        std::getline(in, tmp, '|');
         auto d_name = Varchar<10>::castString(tmp.c_str(), tmp.size());
-        std::getline(in, tmp,'|');
+        std::getline(in, tmp, '|');
         auto d_street_1 = Varchar<20>::castString(tmp.c_str(), tmp.size());
-        std::getline(in, tmp,'|');
+        std::getline(in, tmp, '|');
         auto d_street_2 = Varchar<20>::castString(tmp.c_str(), tmp.size());
-        std::getline(in, tmp,'|');
+        std::getline(in, tmp, '|');
         auto d_city = Varchar<20>::castString(tmp.c_str(), tmp.size());
-        std::getline(in, tmp,'|');
+        std::getline(in, tmp, '|');
         auto d_state = Char<2>::castString(tmp.c_str(), tmp.size());
-        std::getline(in, tmp,'|');
+        std::getline(in, tmp, '|');
         auto d_zip = Char<9>::castString(tmp.c_str(), tmp.size());
-        std::getline(in, tmp,'|');
+        std::getline(in, tmp, '|');
         auto d_tax = Numeric<4, 4>::castString(tmp.c_str(), tmp.size());
-        std::getline(in, tmp,'|');
+        std::getline(in, tmp, '|');
         auto d_ytd = Numeric<12, 2>::castString(tmp.c_str(), tmp.size());
-        std::getline(in, tmp,'\n');
+        std::getline(in, tmp, '\n');
         auto d_next_o_id = Integer::castString(tmp.c_str(), tmp.size());
 
         districtTable.append_row(d_id, d_w_id, d_name, d_street_1, d_street_2, d_city,
-                                  d_state, d_zip, d_tax, d_ytd, d_next_o_id);
+                                 d_state, d_zip, d_tax, d_ytd, d_next_o_id);
     }
 }
 
 void Database::LoadCustomer(std::istream &in) {
-    // TODO
+    std::string tmp = "";
+
+    while (in.peek() != EOF) {
+        std::getline(in, tmp, '|');
+        auto c_id = Integer::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto c_d_id = Integer::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto c_w_id = Integer::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto c_first = Varchar<16>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto c_middle = Char<2>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto c_last = Varchar<16>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto c_street_1 = Varchar<20>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto c_street_2 = Varchar<20>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto c_city = Varchar<20>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto c_state = Char<2>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto c_zip = Char<9>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto c_phone = Char<16>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto c_since = Timestamp::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto c_credit = Char<2>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto c_credit_lim = Numeric<12, 2>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto c_discount = Numeric<4, 4>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto c_balance = Numeric<12, 2>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto c_ytd_paymenr = Numeric<12, 2>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto c_payment_cnt = Numeric<4, 0>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto c_delivery_cnt = Numeric<4, 0>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '\n');
+        auto c_data = Varchar<500>::castString(tmp.c_str(), tmp.size());
+
+        customerTable.append_row(c_id, c_d_id, c_w_id, c_first, c_middle, c_last,
+                c_street_1, c_street_2, c_city, c_state, c_zip, c_phone, c_since,
+                c_credit, c_credit_lim, c_discount, c_balance, c_ytd_paymenr,
+                c_payment_cnt, c_delivery_cnt, c_data);
+    }
 }
 
 void Database::LoadHistory(std::istream &in) {
-    // TODO
+    std::string tmp = "";
+
+    while (in.peek() != EOF) {
+        std::getline(in, tmp, '|');
+        auto h_c_id = Integer::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto h_c_d_id = Integer::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto h_c_w_id = Integer::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto h_d_id = Integer::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto h_w_id = Integer::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto h_date = Timestamp::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto h_amount = Numeric<6, 2>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto h_data = Varchar<24>::castString(tmp.c_str(), tmp.size());
+
+        historyTable.append_row(h_c_id, h_c_d_id, h_c_w_id, h_d_id, h_w_id, h_date,
+                h_amount, h_data);
+    }
 }
 
 void Database::LoadNewOrder(std::istream &in) {
     std::string tmp = "";
 
-    while(in.peek() != EOF) {
-        std::getline(in, tmp,'|');
+    while (in.peek() != EOF) {
+        std::getline(in, tmp, '|');
         auto no_o_id = Integer::castString(tmp.c_str(), tmp.size());
-        std::getline(in, tmp,'|');
+        std::getline(in, tmp, '|');
         auto no_d_id = Integer::castString(tmp.c_str(), tmp.size());
-        std::getline(in, tmp,'\n');
+        std::getline(in, tmp, '\n');
         auto no_w_id = Integer::castString(tmp.c_str(), tmp.size());
 
         neworderTable.append_row(no_o_id, no_d_id, no_w_id);
@@ -93,34 +166,123 @@ void Database::LoadNewOrder(std::istream &in) {
 }
 
 void Database::LoadOrder(std::istream &in) {
-    // TODO
+    std::string tmp = "";
+
+    while (in.peek() != EOF) {
+        std::getline(in, tmp, '|');
+        auto o_id = Integer::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto o_d_id = Integer::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto o_w_id = Integer::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto o_c_id = Integer::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto o_entry_d = Timestamp::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto o_carrier_id = Integer::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto o_ol_cnt = Numeric<2, 0>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto o_all_local = Numeric<1, 0>::castString(tmp.c_str(), tmp.size());
+
+        orderTable.append_row(o_id, o_d_id, o_w_id, o_c_id, o_entry_d, o_carrier_id,
+                o_ol_cnt, o_all_local);
+    }
 }
 
 void Database::LoadOrderLine(std::istream &in) {
-    // TODO
+    std::string tmp = "";
+
+    while (in.peek() != EOF) {
+        std::getline(in, tmp, '|');
+        auto ol_o_id = Integer::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto ol_d_id = Integer::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto ol_w_id = Integer::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto ol_number = Integer::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto ol_i_id = Integer::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto ol_supply_w_id = Integer::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto ol_delivery_d = Timestamp::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto ol_quantity = Numeric<2, 0>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto ol_amount = Numeric<6, 2>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto ol_dist_info = Char<24>::castString(tmp.c_str(), tmp.size());
+
+        orderlineTable.append_row(ol_o_id, ol_d_id, ol_w_id, ol_number, ol_i_id,
+                ol_supply_w_id, ol_delivery_d, ol_quantity, ol_amount, ol_dist_info);
+    }
 }
 
 void Database::LoadItem(std::istream &in) {
     std::string tmp = "";
 
-    while(in.peek() != EOF) {
-        std::getline(in, tmp,'|');
+    while (in.peek() != EOF) {
+        std::getline(in, tmp, '|');
         auto i_id = Integer::castString(tmp.c_str(), tmp.size());
-        std::getline(in, tmp,'|');
+        std::getline(in, tmp, '|');
         auto i_im_id = Integer::castString(tmp.c_str(), tmp.size());
-        std::getline(in, tmp,'|');
+        std::getline(in, tmp, '|');
         auto i_name = Varchar<24>::castString(tmp.c_str(), tmp.size());
-        std::getline(in, tmp,'|');
+        std::getline(in, tmp, '|');
         auto i_price = Numeric<5, 2>::castString(tmp.c_str(), tmp.size());
-        std::getline(in, tmp,'\n');
+        std::getline(in, tmp, '\n');
         auto i_data = Varchar<50>::castString(tmp.c_str(), tmp.size());
 
         itemTable.append_row(i_id, i_im_id, i_name, i_price, i_data);
     }
 }
 
-void Database::LoadStock(std::istream& in) {
-    // TODO
+void Database::LoadStock(std::istream &in) {
+    std::string tmp = "";
+
+    while (in.peek() != EOF) {
+        std::getline(in, tmp, '|');
+        auto s_i_id = Integer::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto s_w_id = Integer::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto s_quantity = Numeric<4, 0>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto s_dist_01 = Char<24>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto s_dist_02 = Char<24>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto s_dist_03 = Char<24>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto s_dist_04 = Char<24>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto s_dist_05 = Char<24>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto s_dist_06 = Char<24>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto s_dist_07 = Char<24>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto s_dist_08 = Char<24>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto s_dist_09 = Char<24>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto s_dist_10 = Char<24>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto s_ytd = Numeric<8, 0>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto s_order_cnt = Numeric<4, 0>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '|');
+        auto s_remote_cnt = Numeric<4, 0>::castString(tmp.c_str(), tmp.size());
+        std::getline(in, tmp, '\n');
+        auto s_data = Varchar<50>::castString(tmp.c_str(), tmp.size());
+
+        stockTable.append_row(s_i_id, s_w_id, s_quantity, s_dist_01, s_dist_02,
+                              s_dist_03, s_dist_04, s_dist_05, s_dist_06, s_dist_07, s_dist_08,
+                              s_dist_09, s_dist_10, s_ytd, s_order_cnt, s_remote_cnt, s_data);
+    }
 }
 
 void Database::NewOrder(
@@ -141,14 +303,14 @@ void Database::NewOrder(
     districtTable.d_next_o_id[districtTable.index_prim_key.at(Key(w_id, d_id))] = o_id + Integer(1);
 
     int all_local = 1;
-    for(int index = 0; index < items.value; index++) {
-        if(w_id != supware[index])
+    for (int index = 0; index < items.value; index++) {
+        if (w_id != supware[index])
             all_local = 0;
     }
 
     // TODO: inserts
 
-    for(int index = 0; index < items.value; index++) {
+    for (int index = 0; index < items.value; index++) {
         auto i_price = itemTable.i_price[itemTable.index_prim_key.at(Key(itemid[index]))];
         auto s_quantity = stockTable.s_quantity[stockTable.index_prim_key.at(Key(supware[index], itemid[index]))];
         auto s_remote_cnt = stockTable.s_remote_cnt[stockTable.index_prim_key.at(Key(supware[index], itemid[index]))];
