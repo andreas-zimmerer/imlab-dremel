da086ec442f71485950884fa95805745ff2f850c
Andreas Zimmerer <andreas.zimmerer@tum.de>
2019-10-27 10:40:02 +0100

    also generate code for primary indexes

--- tools/schemac/schema_compiler.cc
+++ tools/schemac/schema_compiler.cc
@@ -18,19 +18,38 @@ void SchemaCompiler::Compile(Schema &schema) {
 }
 
 void SchemaCompiler::createHeader(Schema &schema) {
-    header_ << "#include <vector>" << std::endl << std::endl;
+    header_ << "#ifndef INCLUDE_IMLAB_SCHEMA_H_" << std::endl;
+    header_ << "#define INCLUDE_IMLAB_SCHEMA_H_" << std::endl;
+    header_ << std::endl;
+
+    header_ << "#include <vector>" << std::endl;
+    header_ << "#include <unordered_map>" << std::endl;
+    header_ << "#include \"imlab/infra/hash.h\"" << std::endl;
+    header_ << std::endl;
 
     for(auto& table : schema.tables) {
         generateTableStruct(table);
         header_ << std::endl;
     }
+
+    header_ << "#endif  // INCLUDE_IMLAB_SCHEMA_H_" << std::endl;
 }
 
 void SchemaCompiler::generateTableStruct(Table &table) {
     header_ << "struct " << table.id << "{" << std::endl;
     for(auto& column : table.columns) {
-        header_ << "    std::vector<" << generateTypeName(column.type) << "> " << column.id << "," << std::endl;
+        header_ << "    std::vector<" << generateTypeName(column.type) << "> " << column.id << ";" << std::endl;
     }
+
+    if(table.primary_key.size() > 0) {
+        header_ << std::endl;
+        header_ << "    std::unordered_map<Key<";
+        for(auto& column : table.primary_key) {
+            header_ << generateTypeName(column.type) << ",";
+        }
+        header_ << ">> primary_key;" << std::endl;
+    }
+
     header_ << "}" << std::endl;
 }
 
