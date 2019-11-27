daa1b76d37c226b73a016f7625da89f8f92facd6
Andreas Zimmerer <andreas.zimmerer@tum.de>
2019-11-27 23:34:30 +0100

    add TBB task_scheduler_init to tests

--- test/tester.cc
+++ test/tester.cc
@@ -3,8 +3,12 @@
 // ---------------------------------------------------------------------------
 
 #include <gtest/gtest.h>
+#include "tbb/task_scheduler_init.h"
 
 int main(int argc, char *argv[]) {
+    // add a tbb::task_scheduler_init, because otherwise the sanitizer will complain about leaks inside TBB.
+    tbb::task_scheduler_init init;
+
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
