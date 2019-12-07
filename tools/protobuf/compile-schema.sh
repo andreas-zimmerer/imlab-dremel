8501f6568ffd5f8822647fe52539ff6c8767be78
Andreas Zimmerer <andreas.zimmerer@tum.de>
2019-12-07 21:07:39 +0100

    move generated schema files to correct location

--- tools/protobuf/compile-schema.sh
+++ tools/protobuf/compile-schema.sh
@@ -3,3 +3,6 @@
 # Make sure you have protobuf installed on your system
 
 protoc --plugin=protoc-gen-schema=./protoc-gen-schema.py --proto_path=../.. --schema_out=./gen ../../data/schema.proto
+
+mv ./gen/schema.cc ../../src/imlab/schema.cc
+mv ./gen/schema.h ../../include/imlab/schema.h
