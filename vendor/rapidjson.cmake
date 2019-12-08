8db4b96b0687dd86c7395f41c81951625fb843ff
Andreas Zimmerer <andreas.zimmerer@tum.de>
2019-12-08 22:56:02 +0100

    fixed rapidjson build

--- vendor/rapidjson.cmake
+++ vendor/rapidjson.cmake
@@ -7,7 +7,8 @@ find_package(Git REQUIRED)
 
 # Rapidjson has some problems with Clang>=5
 # See https://bugs.freebsd.org/bugzilla/show_bug.cgi?id=220388
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-zero-as-null-pointer-constant -Wno-shadow -Wno-exceptions -Wno-implicit-fallthrough -Wno-class-memaccess -Wno-format-overflow")
+# Disable all warnings:
+set(CMAKE_CXX_FLAGS_RAPIDJSON "${CMAKE_CXX_FLAGS} -w")
 
 # Get rapidjson
 ExternalProject_Add(
@@ -20,15 +21,15 @@ ExternalProject_Add(
         -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/vendor/rapidjson
         -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
         -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-        -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
+        -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS_RAPIDJSON}
     UPDATE_COMMAND ""
-    BUILD_BYPRODUCTS <INSTALL_DIR>/lib/librapidjson.a
+    BUILD_BYPRODUCTS <INSTALL_DIR>/lib/libgtest.a
 )
 
 # Prepare rapidjson
 ExternalProject_Get_Property(rapidjson_src install_dir)
 set(RAPIDJSON_INCLUDE_DIR ${install_dir}/include)
-set(RAPIDJSON_LIBRARY_PATH ${install_dir}/lib/librapidjson.a)
+set(RAPIDJSON_LIBRARY_PATH ${install_dir}/lib/libgtest.a)
 file(MAKE_DIRECTORY ${RAPIDJSON_INCLUDE_DIR})
 add_library(rapidjson STATIC IMPORTED)
 set_property(TARGET rapidjson PROPERTY IMPORTED_LOCATION ${RAPIDJSON_LIBRARY_PATH})
