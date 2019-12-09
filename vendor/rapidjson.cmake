749b615aed42417766786c5abf276bea2a265d8c
Andreas Zimmerer <andreas.zimmerer@tum.de>
2019-12-09 10:28:50 +0100

    maybe now with included headers from rapidjson

--- vendor/rapidjson.cmake
+++ vendor/rapidjson.cmake
@@ -12,6 +12,10 @@ ExternalProject_Add(
     GIT_REPOSITORY "https://github.com/Tencent/rapidjson.git"
     GIT_TAG f54b0e47a08782a6131cc3d60f94d038fa6e0a51
     TIMEOUT 10
+    CMAKE_ARGS
+        -DRAPIDJSON_BUILD_TESTS=OFF
+        -DRAPIDJSON_BUILD_DOC=OFF
+        -DRAPIDJSON_BUILD_EXAMPLES=OFF
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
     INSTALL_COMMAND ""
@@ -19,5 +23,5 @@ ExternalProject_Add(
 )
 
 # Prepare rapidjson (rapidjson is a header-only library)
-ExternalProject_Get_Property(rapidjson install_dir)
-set(RAPIDJSON_INCLUDE_DIR ${install_dir}/include)
+ExternalProject_Get_Property(rapidjson source_dir)
+set(RAPIDJSON_INCLUDE_DIR ${source_dir}/include)
