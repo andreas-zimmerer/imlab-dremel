55b841e013093b266f8cf81221006585532f6990
Andreas Zimmerer <andreas.zimmerer@tum.de>
2019-12-11 14:56:54 +0100

    store null values implicitly in DremelVector

--- include/imlab/infra/dremel.h
+++ include/imlab/infra/dremel.h
@@ -4,6 +4,7 @@
 #ifndef INCLUDE_IMLAB_INFRA_DREMEL_H_
 #define INCLUDE_IMLAB_INFRA_DREMEL_H_
 //---------------------------------------------------------------------------
+#include <utility>
 #include <vector>
 #include <string>
 #include <optional>
@@ -19,7 +20,7 @@ class TableBase {
 };
 
 /// A row in Dremel consists of
-///  * a value
+///  * a value that might be null
 ///  * a repetition level
 ///  * a definition level
 template<typename T>
@@ -32,36 +33,49 @@ struct DremelRow {
 /// A column in the Dremel format.
 /// What's special about a Dremel column is that not only values are stored,
 /// but also repetition and definition levels for every value.
-/// Values can also be NULL. This is required for reconstructing the object.
+/// Values can also be NULL.
 ///
 /// Usually, a column is stored a little more efficient in Dremel,
 /// e.g. only required repetition and definition levels are stored while others
 /// are inferred. Also, r and d levels only take up as many bits as they actually
 /// need.
-template<typename T>
+///
+/// \tparam T The type of the values stored in the column.
+/// \tparam max_definition_level The number of optional and repeated fields in the record path.
+///                              Note that 'required' fields should not be counted!
+template<typename T, std::size_t max_definition_level>
 class DremelColumn {
  public:
     /// Creates a new column with a human-readable identifier.
-    explicit DremelColumn(std::string identifier) : identifier(identifier) {}
+    explicit DremelColumn(std::string identifier) : identifier(std::move(identifier)) {}
 
     /// Returns the human-readable identifier for this column.
-    std::string get_identifier() { return identifier; }
+    std::string& get_identifier() { return identifier; }
 
     /// Insert a new value into the column with a given repetition and definition level.
     /// Returns the TID of the inserted value.
     uint64_t insert(DremelRow<T> row) {
-        rows.push_back(row);
+        rows.push_back({ row.value.value_or(nullptr), row.repetition_level, row.definition_level });
         return rows.size() - 1;
     }
 
     /// Retrieves a value together with its repetition and definition levels for a given TID.
     DremelRow<T> get(uint64_t tid) {
-        return rows[tid];
+        auto& [value, r, d] = rows[tid];
+        if (d < max_definition_level) {
+          return { std::nullopt, r, d};
+        } else {
+          return { value, r, d};
+        }
     }
 
  protected:
+    /// Human-readable identifier of this column.
     std::string identifier;
-    std::vector<DremelRow<T>> rows;
+
+    /// Null values are stored implicitly if the definition
+    /// level is smaller than the maximum definition level.
+    std::vector<std::tuple<T, unsigned, unsigned>> rows;
 };
 //---------------------------------------------------------------------------
 }  // namespace imlab
