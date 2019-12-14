e2839ec0f501acf7f13c376fc9a8a81d6446f1fa
Andreas Zimmerer <andreas.zimmerer@tum.de>
2019-12-14 21:41:15 +0100

    cmake file for generated proto_schema files

--- tools/protobuf/local.cmake
+++ tools/protobuf/local.cmake
@@ -2,29 +2,24 @@
 # IMLAB
 # ---------------------------------------------------------------------------
 
-include("${CMAKE_SOURCE_DIR}/tools/protobuf/local.cmake")
-include("${CMAKE_SOURCE_DIR}/tools/schemac/local.cmake")
-include("${CMAKE_SOURCE_DIR}/tools/queryc/local.cmake")
-
 # ---------------------------------------------------------------------------
-# Sources
+# Files
 # ---------------------------------------------------------------------------
 
-set(TOOLS_SRC
-    "tools/imlabdb.cc"
-)
+file(GLOB_RECURSE INCLUDE_H "tools/protobuf/gen/*.h")
+file(GLOB_RECURSE SRC_CC "tools/protobuf/gen/*.cc")
 
 # ---------------------------------------------------------------------------
-# Executables
+# Libraries
 # ---------------------------------------------------------------------------
 
-add_executable(imlabdb tools/imlabdb.cc)
-target_link_libraries(imlabdb imlab tbb query schema dl gflags Threads::Threads)
+add_library(proto_schema STATIC ${SRC_CC})
+target_compile_options(proto_schema PUBLIC "-fPIC")
+target_link_libraries(proto_schema ${PROTOBUF_LIBRARY} gflags Threads::Threads)
 
 # ---------------------------------------------------------------------------
 # Linting
 # ---------------------------------------------------------------------------
 
-add_cpplint_target(lint_tools "${TOOLS_SRC}")
-list(APPEND lint_targets lint_tools)
-
+add_cpplint_target(lint_proto_schema "${INCLUDE_H}")
+list(APPEND lint_targets lint_proto_schema)
