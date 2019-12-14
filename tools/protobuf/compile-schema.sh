5043a303d1a06844b8bf885cbc44dea0406b6909
Andreas Zimmerer <andreas.zimmerer@tum.de>
2019-12-14 21:40:36 +0100

    leave compiled schema files in ./gen/ directory

--- tools/protobuf/compile-schema.sh
+++ tools/protobuf/compile-schema.sh
@@ -3,11 +3,5 @@
 # Make sure you have protobuf installed on your system
 
 # Create a database schema
-protoc --plugin=protoc-gen-schema=./protoc-gen-schema.py --proto_path=../.. --schema_out=./gen ../../data/schema.proto
-protoc --proto_path=../.. --cpp_out=./gen ../../data/schema.proto
-
-mv ./gen/data/*.pb.h ../../include/imlab/schema_types.h
-mv ./gen/data/*.pb.cc ../../src/imlab/schema_types.cc
-
-mv ./gen/data/*.h ../../include/imlab/
-mv ./gen/data/*.cc ../../src/imlab/
+protoc --plugin=protoc-gen-schema=./protoc-gen-schema.py --proto_path=../../data --schema_out=./gen ../../data/schema.proto
+protoc --proto_path=../../data --cpp_out=./gen ../../data/schema.proto
