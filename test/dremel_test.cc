11db73705c5087b30f51460d9308bc74c226e5a9
Andreas Zimmerer <andreas.zimmerer@tum.de>
2019-12-24 19:29:39 +0100

    Print contents of columns in Dremel shredding tests

--- test/dremel_test.cc
+++ test/dremel_test.cc
@@ -12,13 +12,35 @@
 namespace {
 using namespace imlab::dremel;
 
-// Checking the contents of the private vectors
+// Checking the contents of the private vectors and print contents of columns.
 class TestClass : public imlab::schema::DocumentTable {
-    FRIEND_TEST(DatabaseLoadingTest, LoadDremelDocumentTableSmall);
-    FRIEND_TEST(DatabaseLoadingTest, LoadDremelDocumentTableLarge);
+    FRIEND_TEST(DremelTest, ShreddingDocumentRecordSmall);
+    FRIEND_TEST(DremelTest, ShreddingDocumentRecordLarge);
+
+    template<typename T>
+    void _print(DremelColumn<T> column) {
+        std::cout << column.get_identifier() << std::endl;
+        for (uint64_t tid = 0; tid < column.get_size(); tid++) {
+            const auto& row = column.get(tid);
+            if (row.value.has_value())
+                std::cout << "| " << row.value.value() << " | " << row.repetition_level << " | " << row.definition_level << " |" << std::endl;
+            else
+                std::cout << "| " << "NULL" << " | " << row.repetition_level << " | " << row.definition_level << " |" << std::endl;
+        }
+        std::cout << std::endl;
+    }
+
+    void print() {
+        _print(DocId);
+        _print(Links_Backward);
+        _print(Links_Forward);
+        _print(Name_Language_Code);
+        _print(Name_Language_Country);
+        _print(Name_Url);
+    }
 };
 
-TEST(DatabaseLoadingTest, LoadDremelDocumentTableSmall) {
+TEST(DremelTest, ShreddingDocumentRecordSmall) {
     imlab::Database db;
     std::stringstream in(imlab_test::kTestDocumentSmall);
     db.LoadDocumentTable(in);
@@ -39,9 +61,11 @@ TEST(DatabaseLoadingTest, LoadDremelDocumentTableSmall) {
     ASSERT_EQ(tc->Name_Language_Country.get(0), (DremelRow<Varchar<30>>{std::nullopt, 0, 1}));
     ASSERT_EQ(tc->Name_Url.get_size(), 1);
     ASSERT_EQ(tc->Name_Url.get(0), (DremelRow<Varchar<30>>{{Varchar<30>::build("http://C")}, 0, 2}));
+
+    tc->print();
 }
 
-TEST(DatabaseLoadingTest, LoadDremelDocumentTableLarge) {
+TEST(DremelTest, ShreddingDocumentRecordLarge) {
     imlab::Database db;
     std::stringstream in(imlab_test::kTestDocumentLarge);
     db.LoadDocumentTable(in);
@@ -71,6 +95,8 @@ TEST(DatabaseLoadingTest, LoadDremelDocumentTableLarge) {
     ASSERT_EQ(tc->Name_Url.get(0), (DremelRow<Varchar<30>>{{Varchar<30>::build("http://A")}, 0, 2}));
     ASSERT_EQ(tc->Name_Url.get(1), (DremelRow<Varchar<30>>{{Varchar<30>::build("http://B")}, 1, 2}));
     ASSERT_EQ(tc->Name_Url.get(2), (DremelRow<Varchar<30>>{std::nullopt, 1, 1}));
+
+    tc->print();
 }
 
 }  // namespace
