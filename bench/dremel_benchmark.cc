3909285277510c8dd38e0399fb77c779e996f9e9
Andreas Zimmerer <andreas.zimmerer@tum.de>
2019-12-25 21:45:07 +0100

    Benchmark for FSM construction

--- bench/dremel_benchmark.cc
+++ bench/dremel_benchmark.cc
@@ -6,14 +6,23 @@
 #include <chrono>  // NOLINT
 #include <iostream>
 #include <fstream>
+#include "../tools/protobuf/gen/schema.h"
+#include "imlab/dremel/record_fsm.h"
 #include "benchmark/benchmark.h"
 
 namespace {
+using namespace imlab::dremel;
 
-void Benchmark(benchmark::State &state) {
+void BM_Construct_Complete_FSM(benchmark::State &state) {
+    auto* DocId = Document::descriptor()->FindFieldByName("DocId");
+    auto* Links_Backward = Document_Links::descriptor()->FindFieldByName("Backward");
+    auto* Links_Forward = Document_Links::descriptor()->FindFieldByName("Forward");
+    auto* Name_Language_Code = Document_Name_Language::descriptor()->FindFieldByName("Code");
+    auto* Name_Language_Country = Document_Name_Language::descriptor()->FindFieldByName("Country");
+    auto* Name_Url = Document_Name::descriptor()->FindFieldByName("Url");
 
     for (auto _ : state) {
-
+        RecordFSM fsm { std::vector<const FieldDescriptor*>{DocId, Links_Backward, Links_Forward, Name_Language_Code, Name_Language_Country, Name_Url} };
     }
 
     state.SetItemsProcessed(state.iterations());
@@ -21,8 +30,7 @@ void Benchmark(benchmark::State &state) {
 
 }  // namespace
 
-BENCHMARK(Benchmark)
-    ->Iterations(1000000);
+BENCHMARK(BM_Construct_Complete_FSM);
 
 int main(int argc, char** argv) {
     benchmark::Initialize(&argc, argv);
