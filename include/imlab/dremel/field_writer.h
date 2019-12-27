135f6ba140023b537ea379a2ff88fcf431face9a
Andreas Zimmerer <andreas.zimmerer@tum.de>
2019-12-27 20:35:28 +0100

    Now again header-only Dremel implementation

--- include/imlab/dremel/field_writer.h
+++ include/imlab/dremel/field_writer.h
@@ -65,10 +65,6 @@ class ProtoFieldValue {
 };
 
 
-/// Writes a given value into the table with a FieldWriter at a given repetition level.
-void WriteField(FieldWriter* writer, const ProtoFieldValue& value, unsigned repetition_level);
-
-
 /// The base class for a FieldWriter.
 /// FieldWriters will form a tree-like structure that resembles the structure of a record type.
 /// They are used for shredding a nested type into a columnar format.
@@ -133,7 +129,7 @@ template<typename T>
 class AtomicFieldWriter : public FieldWriter {
  public:
     /// Creates a new AtomicFieldWriter that is directly associated with a DremelColumn.
-    AtomicFieldWriter(DremelColumn<T>* column)
+    explicit AtomicFieldWriter(DremelColumn<T>* column)
         : FieldWriter(column->field()), _column(column) {}
     /// Writes an explicitly given value into the column with the given repetition level.
     /// The definition level is the definition level of this writer.
@@ -147,6 +143,44 @@ class AtomicFieldWriter : public FieldWriter {
  private:
     DremelColumn<T>* _column;
 };
+
+
+/// Writes a given value into the table with a FieldWriter at a given repetition level.
+inline void WriteField(FieldWriter* writer, const ProtoFieldValue& value, unsigned repetition_level) {
+    if (!value.HasValue()) {
+        writer->write(repetition_level);
+        return;
+    }
+
+    switch (value.GetField()->cpp_type()) {
+        case FieldDescriptor::CPPTYPE_INT32:
+            dynamic_cast<AtomicFieldWriter<Integer>*>(writer)->write_value(
+                Integer(value.GetFieldValue<int32_t>()), repetition_level);
+            break;
+        case FieldDescriptor::CPPTYPE_INT64:
+            dynamic_cast<AtomicFieldWriter<Integer>*>(writer)->write_value(
+                Integer(value.GetFieldValue<int64_t>()), repetition_level);
+            break;
+        case FieldDescriptor::CPPTYPE_UINT32:
+            dynamic_cast<AtomicFieldWriter<Integer>*>(writer)->write_value(
+                Integer(value.GetFieldValue<uint32_t>()), repetition_level);
+            break;
+        case FieldDescriptor::CPPTYPE_UINT64:
+            dynamic_cast<AtomicFieldWriter<Integer>*>(writer)->write_value(
+                Integer(value.GetFieldValue<uint64_t>()), repetition_level);
+            break;
+        case FieldDescriptor::CPPTYPE_STRING:
+            dynamic_cast<AtomicFieldWriter<Varchar<30>>*>(writer)->write_value(
+                Varchar<30>::build(value.GetFieldValue<std::string>().c_str()), repetition_level);
+            break;
+        case FieldDescriptor::CPPTYPE_DOUBLE:  // UNSUPPRTED
+        case FieldDescriptor::CPPTYPE_FLOAT:  // UNSUPPRTED
+        case FieldDescriptor::CPPTYPE_BOOL:  // UNSUPPRTED
+        case FieldDescriptor::CPPTYPE_ENUM:  // UNSUPPRTED
+        case FieldDescriptor::CPPTYPE_MESSAGE:  // INVALID
+            break;
+    }
+}
 
 //---------------------------------------------------------------------------
 }  // namespace dremel
--- include/imlab/dremel/schema_helper.h
+++ include/imlab/dremel/schema_helper.h
@@ -26,7 +26,7 @@ void AddValue(Message* msg, const FieldDescriptor* field, T value);
 /// Each Descriptor, except for the root, also has a FieldDescriptor associated with it,
 /// but it's kind of hard to get there.
 /// Returns a pointer to the FieldDescriptor or nullptr if we are at the root.
-const FieldDescriptor* GetFieldDescriptor(const Descriptor* desc) {
+inline const FieldDescriptor* GetFieldDescriptor(const Descriptor* desc) {
     auto* parent = desc->containing_type();
     if (parent == nullptr) {  // root
         return nullptr;
@@ -41,7 +41,7 @@ const FieldDescriptor* GetFieldDescriptor(const Descriptor* desc) {
 
 /// Computes the repetition level for a given field.
 /// The repetition level is the number of REPEATED fields in the path.
-unsigned GetMaxRepetitionLevel(const FieldDescriptor* desc) {
+inline unsigned GetMaxRepetitionLevel(const FieldDescriptor* desc) {
     unsigned repetition_level = 0;
 
     auto* field = desc;
@@ -55,7 +55,7 @@ unsigned GetMaxRepetitionLevel(const FieldDescriptor* desc) {
 }
 
 /// Computes the repetition level of the lowest common ancestor of two fields.
-unsigned GetCommonRepetitionLevel(const FieldDescriptor* field1, const FieldDescriptor* field2) {
+inline unsigned GetCommonRepetitionLevel(const FieldDescriptor* field1, const FieldDescriptor* field2) {
     std::vector<const Descriptor*> field1_path {};
     std::vector<const Descriptor*> field2_path {};
 
@@ -83,7 +83,7 @@ unsigned GetCommonRepetitionLevel(const FieldDescriptor* field1, const FieldDesc
 
 /// Computes the definition level of a given field.
 /// The definition level is the number of optional and repeated fields in the path.
-unsigned GetDefinitionLevel(const FieldDescriptor* desc) {
+inline unsigned GetDefinitionLevel(const FieldDescriptor* desc) {
     unsigned definition_level = 0;
 
     auto* field = desc;
@@ -98,7 +98,7 @@ unsigned GetDefinitionLevel(const FieldDescriptor* desc) {
 
 /// In contrast to a "normal" definition level, the full definition level takes
 /// all fields in the path into account.
-unsigned GetFullDefinitionLevel(const FieldDescriptor* desc) {
+inline unsigned GetFullDefinitionLevel(const FieldDescriptor* desc) {
     unsigned full_definition_level = 0;
 
     auto* field = desc;
@@ -107,6 +107,118 @@ unsigned GetFullDefinitionLevel(const FieldDescriptor* desc) {
         field = GetFieldDescriptor(field->containing_type());
     }
     return full_definition_level;
+}
+
+
+
+// ---------------------------------------------------------------------------
+// Type-specific implementations of
+// GetValue<>(), GetRepeatedValue<>(), SetValue<>(), AddValue<>()
+// ---------------------------------------------------------------------------
+template<> inline int32_t GetValue<int32_t>(const Message& msg, const FieldDescriptor* field) {
+    return msg.GetReflection()->GetInt32(msg, field);
+}
+template<> inline int64_t GetValue<int64_t>(const Message& msg, const FieldDescriptor* field) {
+    return msg.GetReflection()->GetInt64(msg, field);
+}
+template<> inline uint32_t GetValue<uint32_t>(const Message& msg, const FieldDescriptor* field) {
+    return msg.GetReflection()->GetUInt32(msg, field);
+}
+template<> inline uint64_t GetValue<uint64_t>(const Message& msg, const FieldDescriptor* field) {
+    return msg.GetReflection()->GetUInt64(msg, field);
+}
+template<> inline double GetValue<double>(const Message& msg, const FieldDescriptor* field) {
+    return msg.GetReflection()->GetDouble(msg, field);
+}
+template<> inline float GetValue<float>(const Message& msg, const FieldDescriptor* field) {
+    return msg.GetReflection()->GetFloat(msg, field);
+}
+template<> inline bool GetValue<bool>(const Message& msg, const FieldDescriptor* field) {
+    return msg.GetReflection()->GetBool(msg, field);
+}
+template<> inline std::string GetValue<std::string>(const Message& msg, const FieldDescriptor* field) {
+    return msg.GetReflection()->GetString(msg, field);
+}
+template<> inline const Message& GetValue<const Message&>(const Message& msg, const FieldDescriptor* field) {
+    return msg.GetReflection()->GetMessage(msg, field);
+}
+// ---------------------------------------------------------------------------
+template<> inline int32_t GetRepeatedValue<int32_t>(const Message& msg, const FieldDescriptor* field, int index) {
+    return msg.GetReflection()->GetRepeatedInt32(msg, field, index);
+}
+template<> inline int64_t GetRepeatedValue<int64_t>(const Message& msg, const FieldDescriptor* field, int index) {
+    return msg.GetReflection()->GetRepeatedInt64(msg, field, index);
+}
+template<> inline uint32_t GetRepeatedValue<uint32_t>(const Message& msg, const FieldDescriptor* field, int index) {
+    return msg.GetReflection()->GetRepeatedUInt32(msg, field, index);
+}
+template<> inline uint64_t GetRepeatedValue<uint64_t>(const Message& msg, const FieldDescriptor* field, int index) {
+    return msg.GetReflection()->GetRepeatedUInt64(msg, field, index);
+}
+template<> inline double GetRepeatedValue<double>(const Message& msg, const FieldDescriptor* field, int index) {
+    return msg.GetReflection()->GetRepeatedDouble(msg, field, index);
+}
+template<> inline float GetRepeatedValue<float>(const Message& msg, const FieldDescriptor* field, int index) {
+    return msg.GetReflection()->GetRepeatedFloat(msg, field, index);
+}
+template<> inline bool GetRepeatedValue<bool>(const Message& msg, const FieldDescriptor* field, int index) {
+    return msg.GetReflection()->GetRepeatedBool(msg, field, index);
+}
+template<> inline std::string GetRepeatedValue<std::string>(const Message& msg, const FieldDescriptor* field, int index) {
+    return msg.GetReflection()->GetRepeatedString(msg, field, index);
+}
+template<> inline const Message& GetRepeatedValue<const Message&>(const Message& msg, const FieldDescriptor* field, int index) {
+    return msg.GetReflection()->GetRepeatedMessage(msg, field, index);
+}
+//---------------------------------------------------------------------------
+template<> inline void SetValue(Message* msg, const FieldDescriptor* field, int32_t value) {
+    msg->GetReflection()->SetInt32(msg, field, value);
+}
+template<> inline void SetValue(Message* msg, const FieldDescriptor* field, int64_t value) {
+    msg->GetReflection()->SetInt64(msg, field, value);
+}
+template<> inline void SetValue(Message* msg, const FieldDescriptor* field, uint32_t value) {
+    msg->GetReflection()->SetUInt32(msg, field, value);
+}
+template<> inline void SetValue(Message* msg, const FieldDescriptor* field, uint64_t value) {
+    msg->GetReflection()->SetUInt64(msg, field, value);
+}
+template<> inline void SetValue(Message* msg, const FieldDescriptor* field, double value) {
+    msg->GetReflection()->SetDouble(msg, field, value);
+}
+template<> inline void SetValue(Message* msg, const FieldDescriptor* field, float value) {
+    msg->GetReflection()->SetFloat(msg, field, value);
+}
+template<> inline void SetValue(Message* msg, const FieldDescriptor* field, bool value) {
+    msg->GetReflection()->SetBool(msg, field, value);
+}
+template<> inline void SetValue(Message* msg, const FieldDescriptor* field, std::string value) {
+    msg->GetReflection()->SetString(msg, field, std::move(value));
+}
+//---------------------------------------------------------------------------
+template<> inline void AddValue(Message* msg, const FieldDescriptor* field, int32_t value) {
+    msg->GetReflection()->AddInt32(msg, field, value);
+}
+template<> inline void AddValue(Message* msg, const FieldDescriptor* field, int64_t value) {
+    msg->GetReflection()->AddInt64(msg, field, value);
+}
+template<> inline void AddValue(Message* msg, const FieldDescriptor* field, uint32_t value) {
+    msg->GetReflection()->AddUInt32(msg, field, value);
+}
+template<> inline void AddValue(Message* msg, const FieldDescriptor* field, uint64_t value) {
+    msg->GetReflection()->AddUInt64(msg, field, value);
+}
+template<> inline void AddValue(Message* msg, const FieldDescriptor* field, double value) {
+    msg->GetReflection()->AddDouble(msg, field, value);
+}
+template<> inline void AddValue(Message* msg, const FieldDescriptor* field, float value) {
+    msg->GetReflection()->AddFloat(msg, field, value);
+}
+template<> inline void AddValue(Message* msg, const FieldDescriptor* field, bool value) {
+    msg->GetReflection()->AddBool(msg, field, value);
+}
+template<> inline void AddValue(Message* msg, const FieldDescriptor* field, std::string value) {
+    msg->GetReflection()->AddString(msg, field, std::move(value));
 }
 
 //---------------------------------------------------------------------------
