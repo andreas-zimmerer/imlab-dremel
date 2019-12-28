9ea8adc1de608eb379a7fc7f0fa3d0868273d56f
Andreas Zimmerer <andreas.zimmerer@tum.de>
2019-12-28 16:43:53 +0100

    Test for roundtrip of insert and get record

--- test/dremel_test.cc
+++ test/dremel_test.cc
@@ -10,6 +10,7 @@
 #include "gtest/gtest.h"
 #include "gtest/gtest_prod.h"
 #include "../include/database.h"
+#include <google/protobuf/util/message_differencer.h>
 
 namespace {
 using namespace imlab::dremel;
@@ -257,6 +258,44 @@ TEST(DremelTest, InsertAndGetSmallRecordWithPartialFSM) {
     std::cout << record.DebugString() << std::endl;
 
     // TODO assert
+}
+
+TEST(DremelTest, InsertAndGetRoundtrip) {
+    // Large record from paper:
+    Document document {};
+    document.set_docid(10);
+    Document_Links* document_links = document.mutable_links();
+    document_links->add_forward(20);
+    document_links->add_forward(40);
+    document_links->add_forward(60);
+    Document_Name* document_name_1 = document.add_name();
+    Document_Name_Language* document_name_1_language_1 = document_name_1->add_language();
+    document_name_1_language_1->set_code("en-us");
+    document_name_1_language_1->set_country("us");
+    Document_Name_Language* document_name_1_language_2 = document_name_1->add_language();
+    document_name_1_language_2->set_code("en");
+    document_name_1->set_url("http://A");
+    Document_Name* document_name_2 = document.add_name();
+    document_name_2->set_url("http://B");
+    Document_Name* document_name_3 = document.add_name();
+    Document_Name_Language* document_name_3_language_1 = document_name_3->add_language();
+    document_name_3_language_1->set_code("en-gb");
+    document_name_3_language_1->set_country("gb");
+
+    auto* DocId = Document::descriptor()->FindFieldByName("DocId");
+    auto* Links_Backward = Document_Links::descriptor()->FindFieldByName("Backward");
+    auto* Links_Forward = Document_Links::descriptor()->FindFieldByName("Forward");
+    auto* Name_Language_Code = Document_Name_Language::descriptor()->FindFieldByName("Code");
+    auto* Name_Language_Country = Document_Name_Language::descriptor()->FindFieldByName("Country");
+    auto* Name_Url = Document_Name::descriptor()->FindFieldByName("Url");
+
+    imlab::Database db {};
+
+    db.documentTable.insert(document);
+    auto record = db.documentTable.get(0, {DocId, Links_Backward, Links_Forward, Name_Language_Code, Name_Language_Country, Name_Url});
+
+    ASSERT_TRUE(google::protobuf::util::MessageDifferencer::Equals(document, record))
+        << "\nExpected Message:\n\n" << document.DebugString() << "\nBut got:\n\n" << record.DebugString();
 }
 
 }  // namespace
